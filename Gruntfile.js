ce937e93ca59eddc014cd16e57242cb51f76db4a
Alex Man <tc.technetium@gmail.com>
2015-02-28 03:21:39 -0800

    Added build script to bundle modules.
    - Debug server
    - Hot reload

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+	var WebPack = require("webpack");
+	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		copy: {
+			app: {
+				src: './src/index.html',
+				dest: './build/index.html'
+			}
+		},
+		webpack: {
+			app: {
+				entry: [ './src/main.js' ],
+				output: {
+					path: './build',
+					filename: 'app.js',
+				},
+				plugins: [
+					new WebPack.optimize.UglifyJsPlugin()
+				],
+			}
+		},
+		'webpack-dev-server': {
+			app: {
+				webpack: {
+					entry: [ 'webpack/hot/dev-server', './src/main.js' ],
+					output: {
+						path: './build',
+						filename: 'app.js',
+					},
+					plugins: [
+						new WebPack.HotModuleReplacementPlugin()
+					],
+				},
+				contentBase: "./build",
+				hot: true,
+				keepAlive: true
+			}
+		},
+		clean: ['./build']
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-webpack');
+	
+	grunt.registerTask('server', ['copy', 'webpack-dev-server']);
+	grunt.registerTask('build', ['copy', 'webpack']);
+	grunt.registerTask('default', ['build']);
+};
