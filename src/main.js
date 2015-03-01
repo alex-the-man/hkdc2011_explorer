1e2904af28a0a15e309fe573562b5e6f0b34265c
Alex Man <tc.technetium@gmail.com>
2015-02-28 20:32:53 -0800

    Webpack dependencies.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
+require('script!../deps/zepto.min.js');
+
+var topojson = require('topojson');
+
 function moveTooltip(x, y) {
-	var tooltipDiv = $("#tooltip");
+	var tooltipDiv = $('#tooltip');
 	tooltipDiv.offset({left: x + 15, top: y + 15});
 }
 
 function updateTooltip(text, x, y) {
-	var tooltipDiv = $("#tooltip");
+	var tooltipDiv = $('#tooltip');
 	tooltipDiv.text(text);
-	if (text == "") {
+	if (text == '') {
 		tooltipDiv.hide();
 	} else {
 		tooltipDiv.show();
@@ -15,19 +19,19 @@ function updateTooltip(text, x, y) {
 }
 
 function loadDCCABoundary(map) {
-	var dccaTopoJson = require("json!./assets/2011dcca.topojson");
-	var dccaGeoJson = topojson.feature(dccaTopoJson, dccaTopoJson.objects["2011dcca"]);
+	var dccaTopoJson = require('json!./assets/2011dcca.topojson');
+	var dccaGeoJson = topojson.feature(dccaTopoJson, dccaTopoJson.objects['2011dcca']);
 	map.data.addGeoJson(dccaGeoJson);
 	map.data.setStyle({
 			strokeWeight: 1.5
 	});
 	map.data.addListener('mouseover', function(event) {
-		var districtName = event.feature.getProperty("CNAME") + " " + event.feature.getProperty("ENAME");
+		var districtName = event.feature.getProperty('CNAME') + ' ' + event.feature.getProperty('ENAME');
 		updateTooltip(districtName, event.kb.clientX, event.kb.clientY);
 		map.data.overrideStyle(event.feature, {fillColor: 'blue'});
 	});
 	map.data.addListener('mouseout', function(event) {
-		updateTooltip("");
+		updateTooltip('');
 		map.data.overrideStyle(event.feature, {fillColor: null});
 	});
 }
